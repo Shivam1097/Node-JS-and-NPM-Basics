3fea25fa0110eb7bc73bd268e9b9cf2888ab13fe
Shivam Shukla <shivamshukla2297@gmail.com>
2019-02-07 19:10:57 +0000

    Installation of NPM package

---
+++ cat_me_package_example/cat_me_npm.js
@@ -0,0 +1,22 @@
+
+// First execute 'npm install cat-me' in the terminal.
+
+// Now include the cat-me package as follows ->
+
+var cats=require("cat-me");
+
+console.log(cats()); 
+// It will print a random cat to console.
+
+
+//  /\     /\
+// {  `---'  }
+// {  O   O  }
+// ~~>  V  <~~
+//  \  \|/  /
+//   `-----'____
+//   /     \    \_
+//  {       }\  )_\_   _
+//  |  \_/  |/ /  \_\_/ )
+//   \__/  /(_/     \__/
+//     (__/
