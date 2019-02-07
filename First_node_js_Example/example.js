31374281a7060b710a0af86e3da2b0f619ff2460
Shivam Shukla <shivamshukla2297@gmail.com>
2019-02-07 18:34:26 +0000

    Running node app.

---
+++ First_node_js_Example/example.js
@@ -0,0 +1,18 @@
+// First install Node.js and add to PATH.
+// Just type 'node <filename.js>' in the terminal and hit enter to execute the the node app.
+
+
+// Average function example
+function avg(arr){
+    var sum=0;
+    arr.forEach(function (arr){
+        sum+=arr;
+    });
+    console.log(Math.round(sum/arr.length));
+}
+
+
+
+
+var num=[40,65,77,82,80,54,73,63,95,49];
+avg(num)
