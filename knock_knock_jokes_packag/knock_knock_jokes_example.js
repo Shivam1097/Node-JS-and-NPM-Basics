1cc21e0c6f9d0c362696895c74d9e81878957837
Shivam Shukla <shivamshukla2297@gmail.com>
2019-02-08 09:46:24 +0000

    knock-knock-jokes-package

---
+++ knock_knock_jokes_packag/knock_knock_jokes_example.js
@@ -0,0 +1,12 @@
+
+var jokes = require("knock-knock-jokes");
+
+console.log(jokes());
+
+// A random joke will print in console.
+
+// Knock, knock.
+// Who’s there?
+// Avenue.
+// Avenue who?
+// Avenue knocked on this door before?
