0b1617866e9e7166fef9d61147e03ad466a358cf
Shivam Shukla <shivamshukla2297@gmail.com>
2019-02-08 13:53:58 +0000

    Initial Commit

---
+++ faker_package_exercise/MyShop/listProducts.js
@@ -0,0 +1,15 @@
+var fake=require("faker");
+
+console.log("=============================================================================================================");
+console.log("                                   Welcome to MyShop                          ");
+console.log("=============================================================================================================");
+
+for(var i=0;i<10;i++){
+    console.log(fake.commerce.productName()+" - $"+fake.commerce.price());     // Generates fake products and fake prices!
+}
+
+
+console.log(fake.name.findName());                  // Generate fake names.
+console.log(fake.internet.email());                 // Generate fake Emails.
+console.log(fake.helpers.createCard());             // Generate fake details of CARD.
+console.log(fake.image.image());                    // Generate fake images.
--- faker_package_exercise/faker_exercise.js
+++
@@ -1,6 +0,0 @@
-var fake=require("faker");
-
-console.log(fake.name.findName());                  // Generate fake names.
-console.log(fake.internet.email());                 // Generate fake Emails.
-console.log(fake.helpers.createCard());             // Generate fake details of CARD.
-console.log(fake.image.image());                    // Generate fake images.
